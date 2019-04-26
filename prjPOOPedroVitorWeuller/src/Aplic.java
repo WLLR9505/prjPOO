4a9a328b02a3c184a6e33a00895dd76b9ebded20
PEDRO BERNARDO DE SOUSA <0030481711006@lti.fatecso.edu.br>
2019-04-26 11:43:06 -0300

    Replace CPFs with fictional values.

--- prjPOOPedroVitorWeuller/src/Aplic.java
+++ prjPOOPedroVitorWeuller/src/Aplic.java
@@ -9,9 +9,9 @@ public class Aplic {
         Produto prod1 = new Produto("1", "Mouse Gamer");
         Produto prod2 = new Produto("2", "Keyboard Iluminado");
 
-        clientes.add(new Cliente("319.587.718-05", "Pedro Bernardo de SOUSA", 900.00));
-        clientes.add(new Cliente("223.861.678-01", "Vitor", 9000.00));
-        clientes.add(new Cliente("458.038.288-90", "Weuller", 52000.00));
+        clientes.add(new Cliente("555.555.555-55", "Pedro Bernardo de SOUSA", 900.00));
+        clientes.add(new Cliente("555.555.555-56", "Vitor", 9000.00));
+        clientes.add(new Cliente("555.555.555-57", "Weuller", 52000.00));
 
         prod1.setEstoqueMinimo(10);
         prod1.setPreco(150.00);
