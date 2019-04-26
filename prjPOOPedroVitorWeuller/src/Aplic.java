7a7b5abf9788491a3f77bcf37919cc94d4262f1d
Weuller Jr. Souza <weuller9505@live.com>
2019-04-26 12:01:58 -0300

    Vendedores teste adicionados

--- prjPOOPedroVitorWeuller/src/Aplic.java
+++ prjPOOPedroVitorWeuller/src/Aplic.java
@@ -2,13 +2,19 @@
 import fatec.poo.model.Cliente;
 import java.util.ArrayList;
 import fatec.poo.model.Produto;
+import fatec.poo.model.Vendedor;
 
 public class Aplic {
     public static void main(String[] args) {
+        ArrayList<Vendedor> vendedores = new ArrayList<Vendedor>();
         ArrayList<Cliente> clientes = new ArrayList<Cliente>();
         Produto prod1 = new Produto("1", "Mouse Gamer");
         Produto prod2 = new Produto("2", "Keyboard Iluminado");
 
+        vendedores.add(new Vendedor("111.111.111-11", "Huguinho", 1200.00));
+        vendedores.add(new Vendedor("222.222.222-22", "Zezinho", 1200.00));
+        vendedores.add(new Vendedor("333.333.333-33", "Luizinho", 1200.00));
+        
         clientes.add(new Cliente("555.555.555-55", "Pedro Bernardo de SOUSA", 900.00));
         clientes.add(new Cliente("555.555.555-56", "Vitor", 9000.00));
         clientes.add(new Cliente("555.555.555-57", "Weuller", 52000.00));
@@ -22,5 +28,11 @@ public class Aplic {
         prod2.setPreco(350.00);
         prod2.setUnidadeMedida("un");
         prod2.setQtdeEstoque(95);
+        
+        for (int i = 0; i < vendedores.size(); i++) {
+            System.out.println("\nCPF: " + vendedores.get(i).getCpf());
+            System.out.println("Vendedor: " + vendedores.get(i).getNome());
+            System.out.println("Salario Base: " + vendedores.get(i).getSalarioBase());
+        }
     }
 }
