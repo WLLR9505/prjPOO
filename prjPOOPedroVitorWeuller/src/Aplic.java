b38835583f3fb7c987e17280be7d7c5165a2aa96
Weuller Jr. Souza <weuller9505@live.com>
2019-04-26 11:37:06 -0300

    Produtos teste adicionados

--- prjPOOPedroVitorWeuller/src/Aplic.java
+++ prjPOOPedroVitorWeuller/src/Aplic.java
@@ -1,6 +1,21 @@
 
+import fatec.poo.model.Produto;
+
+
 public class Aplic {
     public static void main(String[] args) {
+        
+        Produto prod1 = new Produto("1", "Mouse Gamer");
+        prod1.setEstoqueMinimo(10);
+        prod1.setPreco(150.00);
+        prod1.setUnidadeMedida("un");
+        prod1.setQtdeEstoque(95);
+        
+        Produto prod2 = new Produto("2", "Keyboard Iluminado");
+        prod2.setEstoqueMinimo(10);
+        prod2.setPreco(350.00);
+        prod2.setUnidadeMedida("un");
+        prod2.setQtdeEstoque(95);
         
     }
     
