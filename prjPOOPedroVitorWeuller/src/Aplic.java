a72063319a87fee75a739fd467306f454a49462e
PEDRO BERNARDO DE SOUSA <0030481711006@lti.fatecso.edu.br>
2019-04-26 11:38:00 -0300

    Instanciate some clients.

--- prjPOOPedroVitorWeuller/src/Aplic.java
+++ prjPOOPedroVitorWeuller/src/Aplic.java
@@ -1,7 +1,14 @@
 
+import fatec.poo.model.Cliente;
+import java.util.ArrayList;
+
+
 public class Aplic {
     public static void main(String[] args) {
-        
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+        clientes.add(new Cliente("319.587.718-05", "Pedro Bernardo de SOUSA", 900.00));
+        clientes.add(new Cliente("223.861.678-01", "Vitor", 9000.00));
+        clientes.add(new Cliente("458.038.288-90", "Weuller", 52000.00));
     }
-    
 }
